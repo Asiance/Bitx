85b8444609e789705e0a3ccc780f0ec67ba271b1
Laurent Le Graverend <laurent@legraverend.fr>
2013-04-18 15:09:39 +0900

    Analytics + extension name

--- app/js/app.js
+++ app/js/app.js
@@ -17,3 +17,14 @@ angular
         .otherwise({redirectTo: '/'});
     }
   ]);
+
+// How do you use it? We want to lear how to improve it.
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-17785768-10']);
+_gaq.push(['_trackPageview', '/open']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
