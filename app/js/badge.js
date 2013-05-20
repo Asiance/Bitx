99dc89fc491f24c78a91d2ad833058c02ad45e9b
Gilles Piou <gilles.piou@gmail.com>
2013-05-20 10:18:35 +0900

    Add first non-empty counter

--- app/js/badge.js
+++ app/js/badge.js
@@ -16,23 +16,19 @@ function dateToYMD(date) {
  */
 function countTodos(input, status) {
   switch (status) {
-    case "no_due_date":
+    case 'no_due_date':
       return _.where(input, {due_at: null}).length;
-      break;
-    case "today":
+    case 'today':
       return _.where(input, {due_at: dateToYMD(new Date())}).length;
-      break;
-    case "upcoming":
+    case 'upcoming':
       return _.countBy(input, function(todo) {
         return (dateToYMD(new Date(todo.due_at)) > dateToYMD(new Date()));
       }).true;
-      break;
-    case "overdues":
+    case 'overdues':
       return _.countBy(input, function(todo) {
-        if (todo.due_at != null)
+        if (todo.due_at !== null)
           return (dateToYMD(new Date(todo.due_at)) < dateToYMD(new Date()));
       }).true;
-      break;
   }
 }
 
@@ -43,19 +39,41 @@ function countTodos(input, status) {
 function updateBadge(myTodos) {
   try {
     if (myTodos) {
-      var counter_todos = localStorage['counter_todos'];
+      var counter_todos = localStorage.counter_todos;
       var color;
-      counter = countTodos(myTodos, counter_todos);
-      if (!counter) counter = '';
-      if (counter_todos == 'overdues') color = {color: '#F54E4A'};
-      else color = {color: '#5e9ac9'};
+      var counter;
+      if (counter_todos === 'overdues') {
+        color = {color: '#F54E4A'};
+      } else {
+        color = {color: '#5e9ac9'};
+      }
+      if (counter_todos === 'default') {
+        counter = countTodos(myTodos, 'overdues');
+        color = {color: '#F54E4A'};
+        if(!counter) {
+          counter = countTodos(myTodos, 'today');
+          color = {color: '#5e9ac9'};
+          if(!counter) {
+            counter = countTodos(myTodos, 'upcoming');
+            if (!counter) {
+              counter = countTodos(myTodos, 'no_due_date');
+            }
+          }
+        }
+      }
+      else {
+        counter = countTodos(myTodos, counter_todos);
+      }
+      if (!counter) {
+        counter = '';
+      }
       chrome.browserAction.setBadgeBackgroundColor(color);
       chrome.browserAction.setBadgeText({text: counter.toString()});
-      chrome.browserAction.setIcon({path: "./icon.png"});
+      chrome.browserAction.setIcon({path: './icon.png'});
       console.log('LOG: updateBadge');
-      localStorage['updateBadge'] = false;
+      localStorage.updateBadge = false;
     } else {
-      chrome.browserAction.setIcon({path: "./img/icon-inactive.png"});
+      chrome.browserAction.setIcon({path: './img/icon-inactive.png'});
       chrome.browserAction.setBadgeText({text: ""});
     }
   } catch(e) {
