420a1f9aee0499046e85ce9a858dace4be48309a
Adrien Desbiaux <adrien.desbiaux@gmail.com>
2013-08-22 17:52:07 +0900

    karma conf export added and .bat added

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -1,23 +1,27 @@
-basePath = '../';
+module.exports = function(config) {
+  config.set({
+    basePath: '../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  '../app/lib/jquery*.js',
-  '../app/lib/angular.min.js',
-  '../app/lib/angular-*.js',
-  '../app/lib/angular-ui-utils.min.js',
-  '../app/lib/underscore-min.js',
-  'lib/angular-mocks.js',
-  'lib/servicesCacheMock.js',
-  '../app/js/controllers.js',
-  '../app/js/directives.js',
-  '../app/js/filters.js',
-  '../app/js/services.js',
-  '../app/js/lang/en.js',
-  '*-spec.js'
-];
+    files: [
+      JASMINE,
+      JASMINE_ADAPTER,
+      '../app/lib/jquery*.js',
+      '../app/lib/angular.min.js',
+      '../app/lib/angular-*.js',
+      '../app/lib/angular-ui-utils.min.js',
+      '../app/lib/underscore-min.js',
+      'lib/angular-mocks.js',
+      'lib/servicesCacheMock.js',
+      '../app/js/controllers.js',
+      '../app/js/directives.js',
+      '../app/js/filters.js',
+      '../app/js/services.js',
+      '../app/js/lang/en.js',
+      '*-spec.js'
+    ],
 
-autoWatch = true;
+    autoWatch: true,
 
-browsers = ['Chrome'];
+    browsers: ['Chrome']
+  });
+};
