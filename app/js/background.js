59fdb62390bb55f4f7a1851efdaa699e47658488
Gilles Piou <gilles.piou@gmail.com>
2013-11-27 02:11:16 +0100

    Fetch todos from multiple accounts in background

--- app/js/background.js
+++ app/js/background.js
@@ -2,180 +2,171 @@
  * Background script of the Chrome extension
  * Loaded by background.html
  */
-window.onload = function() {
-  var userLang = (navigator.language) ? navigator.language : navigator.userLanguage;
-  var locale = userLang.substring(0,2);
-  var refresh_period;
-  if (!localStorage.language) {
-    localStorage.language = locale;
-  }
-  if (!localStorage.counter_todos) {
-    localStorage.counter_todos = 'default';
-  }
-  if (localStorage.refresh_period) {
-    refresh_period = localStorage.refresh_period;
-  } else {
-    refresh_period = 5000;
-    localStorage.refresh_period = refresh_period;
-  }
-  if (localStorage.myTodos) {
-    var myTodos = JSON.parse(localStorage.myTodos);
-    badge.updateBadge(myTodos);
-  }
-  setInterval(getTodos, refresh_period);
-};
+(function() {
+  'use strict';
 
-/*
- * Retrieve the Basecamp account that supports the last APIs
- * Store data in localStorage
- */
-function getAuthorization() {
-  try {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://launchpad.37signals.com/authorization.json', true);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.basecampToken);
-    xhr.onreadystatechange = function () {
+  var backgroundTasks = {
+
+    basecampToken: localStorage.basecampToken,
+    basecampAccounts: [],
+    userIDs: [],
+    allTodos: [],
+    allTodolists: [],
+    myTodos: [],
+    oldMyTodos: [],
+
+    getBasecampAccounts: function() {
+      var self = this,
+          xhr  = new XMLHttpRequest();
+      xhr.open('GET', 'https://launchpad.37signals.com/authorization.json', false);
+      xhr.setRequestHeader('Authorization', 'Bearer ' + this.basecampToken);
+      xhr.send();
       if (xhr.readyState === 4 && xhr.status === 200) {
         var data = JSON.parse(xhr.responseText);
-        localStorage.basecampId = _.findWhere(data.accounts, {product: "bcx"}).id;
-        getUser();
-        console.log('LOG: getAuthorization XHR');
+        self.basecampAccounts = data.accounts;
+        console.log('LOG: getBasecampAccounts XHR');
       } else if (xhr.readyState === 4) {
         // Token expired
-        console.log('ERROR: getAuthorization XHR - Token expired');
+        console.log('ERROR: getBasecampAccounts XHR - Token expired');
         window.oauth2.renew();
       }
-    };
-    xhr.send();
-  } catch(e) {
-    console.log(e);
-  }
-}
+    },
 
-/*
- * Retrieve the User ID within the Basecamp organization
- * Store data in localStorage
- */
-function getUser() {
-  try {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://basecamp.com/' + localStorage.basecampId + '/api/v1/people/me.json', true);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.basecampToken);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)) {
-        var data = JSON.parse(xhr.responseText);
-        localStorage.userId = data.id;
-        console.log('LOG: getUser XHR');
-      } else if (xhr.readyState === 4) {
-        console.log('ERROR: getUser XHR');
-      }
-    };
-    xhr.send();
-  } catch(e) {
-    console.log(e);
-  }
-}
-
-/*
- * Retrieve the all Todos from projects where user is involved
- * Store data in Chrome storage
- */
-function getTodos() {
-  if (localStorage.basecampToken == undefined) return;
-
-  var allTodos = [];
-  var myTodos = [];
-  var noCache;
-
-  $.ajax({
-    url: 'https://basecamp.com/' + localStorage.basecampId + '/api/v1/todolists.json',
-    headers:  {'Authorization':'Bearer ' + localStorage.basecampToken}
-  })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    getAuthorization();
-  })
-  .done(function(data, textStatus, jqXHR) {
-    chrome.storage.local.get('assignedTodos', function(data) {
-      if (_.isEmpty(data.assignedTodos)) {
-        noCache = true;
-      }
-    });
-    if (jqXHR.getResponseHeader('Status') == '200 OK' || noCache || !localStorage.myTodos || localStorage.updateBadge == 'true') {
-      $.when(asyncEvent(data)).done(function(allTodolists) {
-        _.forEach(allTodolists, function (todolist) {
-          _.forEach(todolist.todos.remaining, function(todo) {
-            todo.todolist = todolist.name;
-            todo.project = todolist.project;
-            todo.project_id = todolist.project_id;
-            allTodos.push(todo);
-            if (todo.assignee && todo.assignee.id == localStorage.userId)
-              myTodos.push(todo);
-          });
-        });
-
-        allTodos = _.chain(allTodos).sortBy(function(todo) { return todo.id; })
-                    .sortBy(function(todo) { return todo.project_id; })
-                    .value();
-        console.log('LOG: getTodos updates cache of allTodos');
-        chrome.storage.local.set({'assignedTodos': JSON.stringify(allTodos)});
-
-        // Create notification
-        if (localStorage.myTodos) {
-          var localMyTodos = JSON.parse(localStorage.myTodos);
-          _.each(myTodos, function(item) {
-            if (!_.findWhere(localMyTodos, {id: item.id})) { // Check each todo whether it is new or not
-              var projectName = _.findWhere(data, {id: item.todolist_id}).bucket.name;
-              var notification = webkitNotifications.createNotification(
-                item.creator.avatar_url, // Icon
-                projectName, // Title
-                item.content // Body
-              );
-              notification.onclick = function () {
-                window.open('https://basecamp.com/' + localStorage.basecampId + '/projects/' + item.project_id + '/todos/' + item.id);
-                notification.close();
-              };
-              notification.show();
-              setTimeout(function() { notification.cancel(); }, 15000); // Hide notificiation after 15 seconds
-            }
-          });
+    getUserIDs: function() {
+      var self = this;
+      _.forEach(this.basecampAccounts, function(basecampAccount) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://basecamp.com/' + basecampAccount.id + '/api/v1/people/me.json', false);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + self.basecampToken);
+        xhr.send();
+        if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)) {
+          var data = JSON.parse(xhr.responseText);
+          self.userIDs.push(data.id);
+        } else if (xhr.readyState === 4) {
+          console.log('ERROR: getUserIDs XHR');
         }
-
-        // Update localStorage
-        localStorage.myTodos = JSON.stringify(myTodos);
-        badge.updateBadge(myTodos);
-        console.log('LOG: getTodos updates cache of myTodos');
       });
+      console.log('LOG: getUserIDs XHR');
+    },
+
+    getTodolists: function() {
+      var self = this;
+      _.forEach(this.basecampAccounts, function(basecampAccount) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://basecamp.com/' + basecampAccount.id + '/api/v1/todolists.json', false);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + self.basecampToken);
+        xhr.send();
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          var data = JSON.parse(xhr.responseText);
+          self.allTodolists.push(data);
+        } else if (xhr.readyState === 4  && xhr.status === 304) {
+          return;
+        } else if (xhr.readyState === 4) {
+          console.log('ERROR: getTodolists XHR');
+        }
+      });
+      console.log('LOG: getTodolists XHR');
+      this.allTodolists = _.flatten(this.allTodolists, true);
+    },
+
+    getTodos: function() {
+      var self = this;
+      _.forEach(this.allTodolists, function(todolist) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', todolist.url, false);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + self.basecampToken);
+        xhr.send();
+        if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)) {
+          var data = JSON.parse(xhr.responseText);
+          self.allTodos.push(data.todos.remaining);
+        } else if (xhr.readyState === 4) {
+          console.log('ERROR: getTodos XHR');
+        }
+      });
+      console.log('LOG: getTodos XHR');
+      self.allTodos = _.flatten(self.allTodos, true);
+    },
+
+    addTodosData: function() {
+      _.map(this.allTodos, function(todo) {
+        var parentTodolist = _.findWhere(this.allTodolists, { id: todo.todolist_id });
+        todo.todolist      = parentTodolist.name ;
+        todo.project       = parentTodolist.bucket.name;
+        todo.project_id    = parentTodolist.bucket.id;
+      }, this);
+    },
+
+    saveTodos: function() {
+      chrome.storage.local.set({ 'assignedTodos': JSON.stringify(this.allTodos) });
+      console.log('LOG: saveTodos updates cache of allTodos');
+    },
+
+    parseMyTodos: function() {
+      var newMyTodos = _.filter(this.allTodos, function(todo) {
+        return todo.assignee && _.contains(this.userIDs, todo.assignee.id);
+      }, this);
+      this.oldMyTodos = localStorage.myTodos ? JSON.parse(localStorage.myTodos) : [];
+      _.map(newMyTodos, function(todo) {
+        if (_.contains(this.oldMyTodos, todo)) return;
+        else this.createNotification(todo);
+      }, this);
+      localStorage.myTodos = JSON.stringify(newMyTodos);
+      badge.updateBadge(newMyTodos);
+      console.log('LOG: parseMyTodos updates cache of myTodos');
+    },
+
+    createNotification: function(todo) {
+      var notification = webkitNotifications.createNotification(
+        todo.creator.avatar_url, // Icon
+        todo.project, // Title
+        todo.content // Body
+      );
+      notification.onclick = function () {
+        // Example of what the replace-regex do:
+        // https://basecamp.com/2457428/api/v1/projects/4324139-explore-basecamp/todos/70309999-click-the-invite.json
+        // https://basecamp.com/2457428/projects/4324139-explore-basecamp/todos/70309999-click-the-invite
+        window.open(todo.url.replace(/[\/]api[\/]v1|[\.]json/gi, ''));
+        notification.close();
+      };
+      notification.show();
+      setTimeout(function() { notification.cancel(); }, 15000); // Hide notificiation after 15 seconds
+    },
+
+    init: function() {
+      var userLang = navigator.language ? navigator.language : navigator.userLanguage,
+          locale   = userLang.substring(0, 2),
+          refresh_period,
+          myTodos;
+      if (!localStorage.language) {
+        localStorage.language = locale;
+      }
+      if (!localStorage.counter_todos) {
+        localStorage.counter_todos = 'default';
+      }
+      if (localStorage.refresh_period) {
+        refresh_period = localStorage.refresh_period;
+      } else {
+        refresh_period = 5000;
+        localStorage.refresh_period = refresh_period;
+      }
+      if (localStorage.myTodos) {
+        myTodos = JSON.parse(localStorage.myTodos);
+        badge.updateBadge(myTodos);
+      }
+    },
+
+    start: function() {
+      this.init();
+      this.getBasecampAccounts();
+      this.getUserIDs();
+      this.getTodolists();
+      this.getTodos();
+      this.addTodosData();
+      this.saveTodos();
+      this.parseMyTodos();
     }
-  });
-}
+  };
 
-function asyncEvent(todolists) {
+  backgroundTasks.start();
 
-  function checkIfModified(status) {
-    if (--done === 0) {
-      return deferred.resolve(allTodolists);
-    }
-  }
-
-  var deferred = new jQuery.Deferred();
-  var done = todolists.length;
-  var allTodolists = [];
-
-  _.each(todolists, function(todolist) {
-    $.ajax({
-      url: todolist.url,
-      headers:  {'Authorization':'Bearer ' + localStorage.basecampToken}
-    }).done(function(data, textStatus, jqXHR) {
-      data.project_id = todolist.bucket.id;
-      data.project = todolist.bucket.name;
-      allTodolists.push(data);
-      checkIfModified();
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-      console.log('ERROR: GET request failed');
-      chrome.storage.local.remove('assignedTodos');
-    });
-  });
-
-  return deferred.promise();
-
-}
+})();
